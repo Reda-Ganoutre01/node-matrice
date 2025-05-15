290ea9067ceb37f4604719e6e2be173e56e02c4d
Reda-Ganoutre01 <bganoutre52@gmail.com>
2025-05-15 14:56:31 +0100

    fix code

--- index.test.js
+++ index.test.js
@@ -2,6 +2,6 @@
 const { sayHello } = require('./index');
 
 test('should greet properly', () => {
-  const result = sayHello("Laraqui");
-  expect(result).toMatch(/Hello Laraqui/);
+  const result = sayHello("Reda Ganoutre");
+  expect(result).toMatch(/Hello Reda Ganoutre/);
 });
